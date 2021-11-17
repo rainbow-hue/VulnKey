77ac68d8a36914b5a7fea26827822491a6c512c3
rainbow-hue <abhineets331@gmail.com>
2021-11-17 16:37:43 +0530

    added form submission

--- xss_scanner.py
+++ xss_scanner.py
@@ -1,11 +1,13 @@
 import requests
 import re
 from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
 class Scanner:
     def __init__(self, url):
         self.target_url = url
         self.target_links = []
+        self.session = requests.Session()
     
     def extract_links(self, url):
         response = requests.get(url)
@@ -14,7 +16,7 @@ class Scanner:
     def crawl(self, url=None):
         if url is None:
             url = self.target_url
-            
+
         href_links = self.extract_links(url)
         for link in href_links:
             link = urljoin(url, link)
@@ -26,5 +28,34 @@ class Scanner:
                 self.target_links.append(link)
                 print(link)
                 self.crawl(link)
-#
+
+    def extract_forms(self, url):
+        response = self.session.get(url)
+
+        parsed_html = BeautifulSoup(response.content)
+        return parsed_html.findAll("forms")
+
+    def submit_form(self, form, value, url):
+        action = form.get("action")
+        post_url = urljoin(url, action)
+        method = form.get("method")
+
+        input_lists = form.findAll("input")
+        post_data = {}
+
+        for input in input_lists:
+            input_name = input.get("name")
+            input_type = input.get("type")
+            input_value = input.get("value")
+
+            if input_type == "text":
+                input_value = value
+            
+            post_data[input_name] = input_value
+            
+        if method == "post":
+            return self.session.post(post_url, data=post_data)
+        return self.session.get(post_url, params=post_data)
+
+
 
